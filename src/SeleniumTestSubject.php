2e52c9bab30af2e2df1daa4d3a3c6c70ccde92cd
Xavier Tô <xavier.to.01@gmail.com>
2010-02-07 09:37:33 -0500

    Added basic functionality for selenium

--- src/SeleniumTestSubject.php
+++ src/SeleniumTestSubject.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'Testing/Selenium.php';
-	
+	require_once 'Expectations.php';
 	class SeleniumTestSubject
 	{
 		
@@ -11,19 +11,26 @@
 		{
 			$this->selenium = new Testing_Selenium($browser,$website);
 			$this->selenium->start();
-			$this->selenium->open($website);
 			return $this;
 		}
 		
-		public function fillsOut($locator)
+		public function goesTo($url)
 		{
-			$this->lastKnownLocator = $locator;
+			$this->selenium->open($url);
+			return $this;	
+		}
+		
+		public function fillsOut($text_field_locator)
+		{
+			$this->lastKnownLocator = $text_field_locator;
 			return $this;
 		}
 		
 		public function with($value)
 		{
+			Expectations::shouldNotBeNull($this->lastKnownLocator,"Nowhere to type... Please specify where to type.");
 			$this->selenium->type($this->lastKnownLocator,$value);
+			$this->lastKnownLocator = null;
 			return $this;
 		}
 		
@@ -41,6 +48,36 @@
 		public function waitsForPageToLoad()
 		{
 			$this->selenium->waitForPageToLoad(30000);
+		}
+		
+		public function drags($objects_locator)
+		{
+			$this->lastKnownLocator = $locator;
+			return $this;
+		}
+		
+		public function dropsOn($destinations_locator)
+		{
+			Expectations::shouldNotBeNull($this->lastKnownLocator,"Nowhere to drop... Please specify where to drop the object.");
+			$this->selenium->dragAndDropToObject($this->lastKnownLocator,$destinations_locator);
+			return $this;
+		}
+		
+		public function checks($checkbox_or_radio_button_locator)
+		{
+			return $this->clicks($checkbox_or_radio_button_locator);
+		}
+		
+		public function selects($value)
+		{
+			$this->lastKnownLocator = $value;
+			return $this;		
+		}
+		
+		public function from($dropdown_list_locator)
+		{
+			Expectations::shouldNotBeNull($this->lastKnownLocator,"Nowhere to pick from... Please specify where to find the selection.");
+			$this->selenium->select($dropdown_list_locator,$this->lastKnownLocator);	
 		}
 		
 	}
