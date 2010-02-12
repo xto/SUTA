3755bb0c97b79b7ba0971ecbae3e07844bd6f724
nicholaslemay <nlemay@pyxis-tech.com>
2010-02-12 11:14:55 -0700

    Added small test for shouldContain expectation.

--- expectations/ExpectationsExpectations.php
+++ expectations/ExpectationsExpectations.php
@@ -66,7 +66,7 @@
         /**
         * @test
         */
-        public function ShouldEqualShouldBehaveLikeAsserEqual()
+        public function ShouldEqualShouldBehaveLikeAssertEqual()
         {
         	self::assertEquals("tom","tom");
         	Expectations::shouldEqual("tom","tom");
@@ -91,6 +91,9 @@
 			
         }
         
+        /**
+        * @test
+        */
         public function ShouldBeNullShouldBehaveLikeAssertNull()
         {
         	Expectations::shouldBeNull(null);
@@ -112,7 +115,10 @@
         	{
         	}
         }
-	 	
+	
+        /**
+        * @test
+        */ 	
         public function ShouldNotBeNullShouldBehaveLikeAssertNotNull()
         {
         	self::assertNotNull("Something not null");
@@ -134,7 +140,16 @@
         	{
         	}
         }
-	 }
+        
+        /**
+        * @test
+        */
+        public function ShouldContainShouldBehaveLikeAssertContains()
+        {
+            self::assertContains("tom","tom petty");
+            Expectations::shouldContain("tom", "tom petty");
+        }
+    }
 
 
 ?>
